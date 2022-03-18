da3a686367d2b92c86cffff5d37ae7c3a5225406
ruither.borba <delki8@gmail.com>
2022-03-18 07:23:55 -0300

    fetching from server

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    fetch('http://pastorais:8080/pastorais/p/pastoral')
+      .then(response => response.json())
+      .then(dat => setData(dat))
+      .catch(e => console.error(e));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {data?.data?.children?.map(({data: topic}) => console.debug(topic) || (
+          <a key={topic.created} href={topic.url}>{topic.title}</a>))}
       </header>
     </div>
   );

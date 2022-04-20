36c94ad97c37b9d4f932004927cae58f9e95d32f
ruither.borba <delki8@gmail.com>
2022-04-20 17:23:43 -0300

    keeping dev url as the default one

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ import {Pastorais} from "./pastorais/Pastorais";
 // localhost
 // export const ROOT_URL = 'http://localhost:9090/pastorais';
 // docker localhost
-// export const ROOT_URL = 'http://localhost:8080/pastorais';
+ export const ROOT_URL = 'http://localhost:8080/pastorais';
 // production
-export const ROOT_URL = 'http://admin.ipmosaico.com:8888/pastorais';
+// export const ROOT_URL = 'http://admin.ipmosaico.com:8888/pastorais';
 
 function App() {
   const { token, setToken } = useToken();

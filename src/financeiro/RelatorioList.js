58c587843947d589125da1665e38ea84c359261a
ruither.borba <delki8@gmail.com>
2022-07-10 10:37:13 -0300

    formatting financeiro numbers and increasing font size

--- src/financeiro/RelatorioList.js
+++ src/financeiro/RelatorioList.js
@@ -18,21 +18,22 @@ export const RelatorioList = ({token, setToken, relatorios, setRelatorios, selec
     }
   }, [token, setRelatorios, relatorios.length]);
 
+  const formatter = new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+      });
+
   return (relatorios?.map(relatorio => (<div key={relatorio.id}>
       <h4>{relatorio.anoMes?.[1]} / {relatorio.anoMes?.[0]}</h4>
-      <label>
-        entradas: {relatorio.entradas}
-      </label>
-      <br/>
-      <label>
-        saídas: {relatorio.saidas}
-      </label>
-      <br/>
-      <label>
-        orçado: {relatorio.orcado}
-      </label>
-      <br/>
-      <br/>
+      <div style={{fontSize: '1.75rem'}}>
+        entradas: <strong>{formatter.format(relatorio.entradas)}</strong>
+        <br/>
+        saídas: <strong>{formatter.format(relatorio.saidas)}</strong>
+        <br/>
+        orçado: <strong>{formatter.format(relatorio.orcado)}</strong>
+        <br/>
+        <br/>
+      </div>
       <div className="grid">
         {token &&
         <SelectAction entity={relatorio} select={selectRelatorio}/>

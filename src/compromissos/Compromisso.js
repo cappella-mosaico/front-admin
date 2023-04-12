bf8f1fb264af4491bd85363031e4b82f2f117ba3
ruither.borba <delki8@gmail.com>
2023-04-12 09:59:40 -0300

    beautify compromissos list

--- src/compromissos/Compromisso.js
+++ src/compromissos/Compromisso.js
@@ -1,3 +1,42 @@
 export const Compromisso = ({ compromisso, token }) => {
-  return (<>{compromisso.id} - {compromisso.nome} - {compromisso.createdAt}<br /></>);
+  console.log(compromisso);
+  const date = new Date(compromisso.inicio);
+  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', timeZone: 'UTC' };
+  const formattedDate = date.toLocaleDateString('pt-BR', options);
+  const formattedTime = date.toLocaleTimeString('pt-BR', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: false });
+  const formattedDateTime = `${formattedDate} ${formattedTime}`;
+  return (<article>
+            <header><strong>{compromisso.tipo} - {compromisso.ministerio}</strong></header>
+            <div className="grid">
+            <table>
+            <tbody>
+              <tr>
+                <td><b>equipes:</b></td>
+                <td>{compromisso.equipes}</td>
+              </tr>
+              <tr>
+                <td><b>nome:</b></td>
+                <td>{compromisso.nome}</td>
+              </tr>
+              <tr>
+                <td><b>inicio:</b></td>
+                <td>{formattedDateTime}</td>
+              </tr>
+              <tr>
+                <td><b>local:</b></td>
+                <td>{compromisso.local}</td>
+              </tr>
+              <tr>
+                <td><b>endereco:</b></td>
+                <td>{compromisso.endereco}</td>
+              </tr>
+          </tbody>
+            </table>
+            <img src={compromisso.imagem} width="300"/>
+            </div>
+            <footer className="grid">
+              <button>editar</button>
+              <button>apagar</button>
+            </footer>
+          </article>);
 }
--- src/compromissos/Compromissos.js
+++ src/compromissos/Compromissos.js
@@ -4,26 +4,21 @@ import { CompromissoForm } from './CompromissoForm';
 
 export const Compromissos = ({ token, setToken }) => {
   const [compromissos, setCompromissos] = useState([]);
-  const addCompromisso = (c) => {
-    setCompromissos([c, ...compromissos]);
-  }
+  const addCompromisso = (c) => setCompromissos([c, ...compromissos]);
 
   return (<>
           <h3>Compromissos e Equipes</h3>
 
           <CompromissoForm
-          token={token}
-          setToken={setToken}
-          addCompromisso={addCompromisso} />
-          <br />
-          formulario de cadastro de equipes
-          <br />
+            token={token}
+            setToken={setToken}
+            addCompromisso={addCompromisso} />
 
           <CompromissosList
-          token={token}
-          compromissos={compromissos}
-          addCompromisso={addCompromisso}
-          setCompromissos={setCompromissos}/>
-
+            token={token}
+            compromissos={compromissos}
+            addCompromisso={addCompromisso}
+            setCompromissos={setCompromissos}/>
           </>);
-}
+
+};
